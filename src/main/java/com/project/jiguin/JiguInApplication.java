aa0ea768810078da396cb8baa38ba7fd5f62666c
hagyoung <hagyoung99@gmail.com>
2023-05-29 11:53:17 +0900

    upload

---
+++ src/main/java/com/project/jiguin/JiguInApplication.java
@@ -0,0 +1,13 @@
+package com.project.jiguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiguInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiguInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/jiguin/JiguInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jiguin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiguInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
